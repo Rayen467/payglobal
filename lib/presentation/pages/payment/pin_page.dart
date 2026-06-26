67d248168ee59f21791c007affd36fb653e1e1de
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-06-26 16:35:19 +0700

    feat(page): update PinPage Scaffold background to AppColors.bg

--- lib/presentation/pages/payment/pin_page.dart
+++ lib/presentation/pages/payment/pin_page.dart
@@ -245,7 +245,7 @@ class _PinPageState extends State<PinPage> {
         ),
       ],
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: AppColors.bg,
         body: SafeArea(
           child: Column(
             children: [
